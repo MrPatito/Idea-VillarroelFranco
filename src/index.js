8b7d3165ffd76061d508d3dc1955b02b81c8fcc4
Franco <franco.hirschfeldt@gmail.com>
2021-08-02 23:53:09 -0300

    incursion en componentes react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/navBar";
 
-ReactDOM. render(
-    <div>Ta fachero ehh...
-    <p>ta facherito eh...</p>
-    </div> ,
-    document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+// ReactDOM. render(
+//     <div>Ta fachero ehh...
+//     <p>ta facherito eh...</p>
+//     </div> ,
+//     document.getElementById('root'))
 
     
