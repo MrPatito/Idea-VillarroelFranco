bce7cfc3351865f9487ef77f60c5a67eed70261d
Franco <franco.hirschfeldt@gmail.com>
2021-08-11 17:37:42 -0300

    prueba usestate

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -1,49 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Counter extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {value: 0};
-        this.buttonClicked = this.buttonClicked.bind(this)
-
-        this.buttonClicked1 = this.buttonClicked1.bind(this)
-    }
-
-    buttonClicked(event){
-        this.setState({value: this.state.value+1})
-    }
-    buttonClicked1(event){
-        this.setState({value: this.state.value-1})
-    }
-
-    render(){
-        return(
-                        // <div className='itemCount'>
-                            <div className='container'>
-                                <p className='heading'>Contador</p>
-                                <p className='counter'>{this.state.value}</p>
-                                <button onClick={this.buttonClicked} className='botonContador' id='incr'>+</button>
-                                <button onClick={this.buttonClicked1} className='botonContador' id='decr'>-</button>
-                            </div>
-                        // </div>
-                        
-                        
-                    );
-    }
-}
-
-export default Counter;
-
-// const Counter = (props) =>{
-//     return(
-//             <div className='container'>
-//                 <p className='heading'>Contador</p>
-//                 <p className='counter'>0</p>
-//                 <button  className='botonContador' id='incr'>+</button>
-//                 <button  className='botonContador' id='decr'>-</button>
-//             </div>
+const Counter = ({stock, botonesActivos, add}) => {
+    const [contador, setContador] = useState(1);
+    
+    stock = 7
+    botonesActivos = false
+    useEffect(()=>{
+        if (contador <= 7 || contador >= 0){
+            botonesActivos = false  
+            console.log("ahre")
+          }
+        else{
+            botonesActivos = true
+          }
+    })
+    
+    
+    return(
+            <div className='container'>
+                <p className='heading'>Contador</p>
+                <p className='counter'>{contador}</p>
+                <button disabled={botonesActivos} onClick={() => setContador(contador -1)} className='botonContador' id='decr'>-</button>
+                <button disabled={botonesActivos} onClick={() => setContador(contador +1)} className='botonContador' id='incr'>+</button>
+            </div>
             
-//         );
+        );
         
-// }
-// export default Counter;
+}
+export default Counter;
