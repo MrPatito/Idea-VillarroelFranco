4f90e0a204f281d29c3387e2705598c9d3da2c15
Franco <franco.hirschfeldt@gmail.com>
2021-08-11 19:40:40 -0300

    correccion de counter e incorporacion de hooks

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -1,27 +1,65 @@
 import React, { useEffect, useState } from 'react';
 
-const Counter = ({stock, botonesActivos, add}) => {
+const Counter = () => {
     const [contador, setContador] = useState(1);
+    const [stock, setStock] = useState(7)
+    // const [btnIncr, setBtnIncr] = useState(false)
+    // const [btnDecr, setBtnDecr] = useState(false)
     
-    stock = 7
-    botonesActivos = false
-    useEffect(()=>{
-        if (contador <= 7 || contador >= 0){
-            botonesActivos = false  
-            console.log("ahre")
-          }
+    // useEffect(()=>{
+        
+    //     if (stock > 0){
+    //         setBtnDecr = false
+    //         setBtnIncr = true 
+
+    //         console.log("ahre")
+    //       }
+    //     else{
+    //         setBtnDecr = true
+    //         setBtnIncr = true 
+    //       }
+    // },) 
+
+
+    function decr (){
+        if(contador > 0 ){
+            setContador(contador -1);
+            setStock(stock + 1)
+            console.log(stock)
+        }
         else{
-            botonesActivos = true
-          }
-    })
+            console.log('ahre')
+        }
+        
+
+        
+    }
+
+    function incr (){
+        if(stock > 0){
+            setContador(contador + 1);
+            setStock(stock - 1)
+            console.log(stock)
+
+        }
+        else{
+            console.log('ahre lak')
+        }
+        
+        
+    }
+
+    
+
+    
     
     
     return(
             <div className='container'>
                 <p className='heading'>Contador</p>
                 <p className='counter'>{contador}</p>
-                <button disabled={botonesActivos} onClick={() => setContador(contador -1)} className='botonContador' id='decr'>-</button>
-                <button disabled={botonesActivos} onClick={() => setContador(contador +1)} className='botonContador' id='incr'>+</button>
+                <button onClick={decr} className='botonContador' id='decr'>-</button>
+                <button onClick={incr} className='botonContador' id='incr'>+</button>
             </div>
             
         );
