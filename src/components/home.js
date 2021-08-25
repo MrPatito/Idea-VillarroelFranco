f856f589ca1f4e6b647fd8ddd868a20108f87144
Franco <franco.hirschfeldt@gmail.com>
2021-08-25 03:46:05 -0300

    routing
    agregando routing dinamico

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Home = () =>{
+    const history = useHistory();
+
+    const id = 152
+    const pagar = () => history.push(`/category/${id}`);
+
     return(
-        <div>
+        <div className='listItemcContainer'>
             <h1>Hola. Soy un HOME</h1>
+            <button onClick={pagar}>pagar</button>
         </div>
     )
 }
